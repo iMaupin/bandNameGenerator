be01606f6f8e6c681bc7154e8cda6198c5ac92ce
Isaac Maupin <isaac@boardvitals.com>
2021-11-26 11:05:06 -0500

    initial commit

---
+++ bandNameGenerator.py
@@ -0,0 +1,4 @@
+print('Hello and welcome to the band name generator!\nPlease follow the prompts to create a band name.')
+city = input('What city did you grow up in?\n')
+pet_name = input('What is the name of your pet?\n')
+print(f'Your band name is {city} {pet_name}')
