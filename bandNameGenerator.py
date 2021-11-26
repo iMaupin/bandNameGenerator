0c14bb9ab81a888f160cfee84789c6b383005ded
Isaac Maupin <isaac@boardvitals.com>
2021-11-26 11:12:51 -0500

    updated text in in prompt

--- bandNameGenerator.py
+++ bandNameGenerator.py
@@ -1,4 +1,4 @@
-print('Hello and welcome to the band name generator!\nPlease follow the prompts to create a band name.')
+print('Hello and welcome to the band name generator!\nPlease follow the prompts to create a name for your band.')
 city = input('What city did you grow up in?\n')
 pet_name = input('What is the name of your pet?\n')
 print(f'Your band name is {city} {pet_name}')
